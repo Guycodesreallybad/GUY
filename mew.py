eb0db5b8e6c36d21d8ed535e74a0bb2680292c95
Elvis Roman <Adrian.romanjr12@gmail.com>
2024-09-30 13:55:58 -0500

    Add files via upload

---
+++ mew.py
@@ -0,0 +1,10 @@
+# Function definition
+def circle_area(radius):
+    return 3.14159 * radius ** 2
+
+def square_area(side):
+    return side ** 2
+
+# Print statements
+print("The area of the circle is:", circle_area(5))
+print("The area of the square is:", square_area(5))
