671a13fc3aa6c7b301a1b61731fa4afbcf2f14cf
Elvis Roman <Adrian.romanjr12@gmail.com>
2024-10-08 18:05:56 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+# We have a car with a gas tank size of 15 gallons
+tank_size = 15
+
+# The car can travel a distance of 600 miles on a full tank
+distance = 600
+
+# Calculate miles per gallon (mpg) by dividing the distance by the tank size
+mpg = distance / tank_size
+
+# Print out the result, showing how many miles the car can travel per gallon of gas
+print("Miles per gallon:", mpg)
